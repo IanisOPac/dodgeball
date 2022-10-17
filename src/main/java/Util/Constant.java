409ff7034a23db3ee93cea51bec2d58599800874
IanisOPac <ianis.olivier.pacaud@gmail.com>
2022-10-17 19:00:57 +0200

    QOL Constant class + added Constant class file for easily editable game parameters

---
+++ src/main/java/Util/Constant.java
@@ -0,0 +1,8 @@
+package Util;
+
+public class Constant {
+    public static final int WINDOW_WIDTH = 600;
+    public static final int WINDOW_HEIGHT = 600;
+    public static final int NB_PLAYERS = 3;
+
+}
